a8ed7f848717ff18fbef3455c75c95f46b51a857
Augustus <agstrscntw3@gmail.com>
2020-01-07 18:34:44 +1030

    v1.1.0

--- src/VouchersServiceProvider.php
+++ src/VouchersServiceProvider.php
@@ -22,6 +22,7 @@ class VouchersServiceProvider extends ServiceProvider
             if (! class_exists('CreateVouchersTable')) {
                 $this->publishes([
                     __DIR__.'/../database/migrations/create_vouchers_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_vouchers_table.php'),
+                    __DIR__.'/../database/migrations/add_columns_to_vouchers_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_add_columns_to_vouchers_table.php'),
                 ], 'migrations');
             }
 
